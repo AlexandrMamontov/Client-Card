0a50b6b9710d2db4e69a89c34f7810c8c907223a
alexandr_mamontov <sasham13101999@yandex.ru>
2025-07-09 19:31:36 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let tabsBtn = document.querySelectorAll(".header__tab");
+  let tabsBlocks = document.querySelectorAll(".main__wrapper");
+
+  tabsBtn.forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      const path = e.currentTarget.dataset.btn;
+
+      tabsBtn.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
+
+      e.currentTarget.classList.add("active");
+
+      tabsBlocks.forEach(function (element) {
+        element.classList.remove("active");
+      });
+
+      document.querySelector(`[data-content="${path}"]`).classList.add("active");
+    });
+  });
+});
